b2272e48f0c01349a78a69da644e650e219fada0
TusharImran-Dev <tusharimran.dev@gmail.com>
2023-05-10 23:27:04 +0600

    My Portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/*-active nav link on scroll--*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('section #nav-links a')
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('section #nav-links a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
